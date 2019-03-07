c79a2c4a61a79225c0a8e561dad9b84b5fc2cef0
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-03-07 09:18:49 +0100

    Whitespace copy/paste error.

--- network-import/network-import.py
+++ network-import/network-import.py
@@ -145,14 +145,14 @@ def read_tags():
 # From python 3.7 Lib/ipaddress.py.
 def _is_subnet_of(a, b):
     try:
-	# Always false if one is v4 and the other is v6.
-	if a._version != b._version:
-	    raise TypeError(f"{a} and {b} are not of the same version")
-	return (b.network_address <= a.network_address and
-		b.broadcast_address >= a.broadcast_address)
+        # Always false if one is v4 and the other is v6.
+        if a._version != b._version:
+            raise TypeError(f"{a} and {b} are not of the same version")
+        return (b.network_address <= a.network_address and
+                b.broadcast_address >= a.broadcast_address)
     except AttributeError:
-	raise TypeError(f"Unable to test subnet containment "
-			f"between {a} and {b}")
+        raise TypeError(f"Unable to test subnet containment "
+                        f"between {a} and {b}")
 
 def subnet_of(a, b):
     """Return True if this network is a subnet of other."""
