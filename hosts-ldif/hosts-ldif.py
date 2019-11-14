06c2f84f82a3d4bdc847c3b77fdd4cf648d82b46
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-11-14 14:46:18 +0100

    Add script to create a host ldif.
    Currently a bit UiO-specific.
    
    Note getting all hosts is a very heavy operation, as the regular
    HostSerializer includes many extra fields for 1-to-M-relations.

---
+++ hosts-ldif/hosts-ldif.py
@@ -0,0 +1,106 @@
+import argparse
+import configparser
+import io
+import os
+import pathlib
+import sys
+
+import fasteners
+
+import requests
+
+parentdir = pathlib.Path(__file__).resolve().parent.parent
+sys.path.append(str(parentdir))
+import common.connection
+import common.utils
+
+from common.utils import error
+from common.LDIFutils import entry_string, make_head_entry
+
+
+def create_ldif(hosts, ignore_size_change):
+    f = io.StringIO()
+    dn = cfg['ldif']['dn']
+    head_entry = make_head_entry(cfg)
+    f.write(entry_string(head_entry))
+    f.write('\n')
+    for i in hosts:
+        cn = i['name']
+        entry = {
+            'dn': f'cn={cn},{dn}',
+            'host': cn,
+            'objectClass': 'uioHostinfo',
+            'uioHostComment':  i['comment'],
+            'uioHostContact':  i['contact'],
+            }
+        mac = [ip['macaddress'] for ip in i['ipaddresses'] if ip['macaddress']]
+        if mac:
+            entry['uioHostMacAddr'] = mac
+        f.write(entry_string(entry))
+        f.write('\n')
+    try:
+        common.utils.write_file(cfg['default']['filename'], f,
+                                ignore_size_change=ignore_size_change)
+    except common.utils.TooManyLineChanges as e:
+        error(e.message)
+
+
+@common.utils.timing
+def get_hosts(url):
+    return conn.get_list(url + '?page_size=1000&ordering=name')
+
+
+@common.utils.timing
+def hosts_ldif(args, url):
+    for i in ('destdir', 'workdir',):
+        common.utils.mkdir(cfg['default'][i])
+
+    lockfile = os.path.join(cfg['default']['workdir'], 'lockfile')
+    lock = fasteners.InterProcessLock(lockfile)
+    if lock.acquire(blocking=False):
+        if common.utils.updated_entries(conn, url, 'hosts.json') or args.force_check:
+            hosts = get_hosts(url)
+            create_ldif(hosts, args.ignore_size_change)
+            if 'postcommand' in cfg['default']:
+                common.utils.run_postcommand()
+        else:
+            logger.info("No updated hosts")
+        lock.release()
+    else:
+        logger.warning(f"Could not lock on {lockfile}")
+
+
+def main():
+    global cfg, conn, logger
+    parser = argparse.ArgumentParser(description="Export hosts from mreg as a ldif.")
+    parser.add_argument("--config",
+                        default="hosts-ldif.conf",
+                        help="path to config file (default: %(default)s)")
+    parser.add_argument('--force-check',
+                        action='store_true',
+                        help='force refresh of data from mreg')
+    parser.add_argument('--ignore-size-change',
+                        action='store_true',
+                        help='ignore size changes')
+    args = parser.parse_args()
+
+    cfg = configparser.ConfigParser()
+    cfg.optionxform = str
+    cfg.read(args.config)
+
+    for i in ('default', 'mreg', 'ldif'):
+        if i not in cfg:
+            error(f"Missing section {i} in config file", os.EX_CONFIG)
+
+    if 'filename' not in cfg['default']:
+        error(f"Missing 'filename' in default section in config file", os.EX_CONFIG)
+
+    common.utils.cfg = cfg
+    logger = common.utils.getLogger()
+    conn = common.connection.Connection(cfg['mreg'])
+    url = requests.compat.urljoin(cfg["mreg"]["url"], '/api/v1/hosts/')
+    hosts_ldif(args, url)
+
+
+if __name__ == '__main__':
+    main()
