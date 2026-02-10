ca3f67d550b6f06dcfee150208863822f5df6ebc
pederhan <pederhan@usit.uio.no>
2026-02-10 13:08:13 +0100

    Control exc info and logging in exit_err

--- mreg_tools/output/output.py
+++ mreg_tools/output/output.py
@@ -14,7 +14,7 @@ from rich.markup import render
 
 from mreg_tools.output.style import DEFAULT_THEME
 
-logger = structlog.stdlib.get_logger("mreg_tools")
+logger = structlog.stdlib.get_logger()
 
 
 # stdout console used to print results
@@ -135,7 +135,12 @@ def exit_ok(message: str | None = None, code: int | None = 0, **kwargs: Any) -> 
 
 
 def exit_err(
-    message: str, code: int | None = 1, escape: bool = False, **kwargs: Any
+    message: str,
+    code: int | None = 1,
+    escape: bool = False,
+    log: bool = True,
+    exc_info: bool = False,
+    **kwargs: Any,
 ) -> NoReturn:
     """Log a message with ERROR level and exit with the given code (default: 1).
 
@@ -143,9 +148,12 @@ def exit_err(
         message (str): Message to print.
         escape (bool): Whether to escape the message for rich markup. Defaults to False.
         code (int, optional): Exit code. Defaults to 1.
+        log (bool): Whether to log the error message. Defaults to True.
+        exc_info (bool): Whether to include exception info in the log.
+        Enables logging regardless of `log` value. Defaults to False.
         **kwargs: Additional keyword arguments to pass to the extra dict.
     """
     if escape:
         message = escape_markup(message)
-    error(message, **kwargs)
+    error(message, exc_info=exc_info, log=log or exc_info, **kwargs)
     raise SystemExit(code if code is not None else 1)
