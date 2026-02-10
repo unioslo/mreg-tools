769d01e78a106a76898604c7dbf027929fa84f17
pederhan <pederhan@usit.uio.no>
2026-02-10 10:02:42 +0100

    Grammar

--- mreg_tools/api.py
+++ mreg_tools/api.py
@@ -7,17 +7,17 @@ from mreg_tools.output import err_console
 
 
 def _get_client(config: MregConfig) -> MregClient:
-    """Create a MregClient from a MregConfig."""
+    """Create an MregClient from an MregConfig."""
     MregClient.reset_instance()
     return MregClient(
         url=config.url,
-        timeout=600,  # TODO: make configurable?
+        timeout=600,  # TODO: make configurable
         page_size=config.page_size,
     )
 
 
 def get_client_and_login(config: MregConfig) -> MregClient:
-    """Create and log in a MregClient from a MregConfig."""
+    """Create and log in an MregClient from an MregConfig."""
     client = _get_client(config)
     try:
         client.login(username=config.username, password=config.get_password())
