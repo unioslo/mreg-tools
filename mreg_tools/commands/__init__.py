2dc412aa3f9f1a2a33d61bda88c78bf7229f2346
pederhan <pederhan@usit.uio.no>
2026-02-11 16:16:08 +0100

    Add hostgroup-ldif

--- mreg_tools/commands/__init__.py
+++ mreg_tools/commands/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from mreg_tools.commands import hostgroup_ldif
 from mreg_tools.commands import hosts_ldif
 
 __all__ = [
     "hosts_ldif",
+    "hostgroup_ldif",
 ]
--- mreg_tools/commands/hostgroup_ldif.py
+++ mreg_tools/commands/hostgroup_ldif.py
@@ -1,134 +1,175 @@
 from __future__ import annotations
 
-import configparser
 import io
-import os
 from typing import Annotated
+from typing import NotRequired
+from typing import TypedDict
+from typing import override
 
-import fasteners
-import requests
 import typer
+from mreg_api.models import HostGroup
 
 from mreg_tools import common
 from mreg_tools.app import app
+from mreg_tools.common.LDIFutils import LDIFBase
+from mreg_tools.common.LDIFutils import LdifData
+from mreg_tools.common.LDIFutils import LdifDataStorageBase
 from mreg_tools.common.LDIFutils import entry_string
-from mreg_tools.common.LDIFutils import make_head_entry
-from mreg_tools.common.utils import error
+from mreg_tools.config import Config
+from mreg_tools.config import HostGroupLdifConfig
+from mreg_tools.logs import configure_logging
 
 
-def create_ldif(hostgroups):
-    def write_file(filename):
-        common.utils.write_file(filename, f)
+class HostGroupLdifEntry(TypedDict):
+    """Host group LDIF entry structure."""
 
-    f = io.StringIO()
-    if cfg["mreg"].getboolean("make_head_entry"):
-        head_entry = make_head_entry(cfg)
-        f.write(entry_string(head_entry))
-    for entry in create_hostgroupsentries(hostgroups):
-        f.write(entry_string(entry))
-    write_file(cfg["default"]["filename"])
+    dn: str
+    cn: str
+    description: str | None
+    objectClass: list[str]
+    memberNisNetgroup: NotRequired[list[str]]
+    nisNetgroupTriple: NotRequired[list[str]]
 
 
-def get_objectclass():
-    return make_head_entry(cfg)["objectClass"]
+class HostGroupLdifDataStorage(LdifDataStorageBase):
+    """Storage for fetched host group data."""
+
+    def __init__(self, hostgroups: LdifData[HostGroup]) -> None:
+        self.hostgroups = hostgroups
 
 
-def create_hostgroupsentries(hostgroups):
-    ret = []
-    remove_domain = cfg["mreg"].get("domain", None)
-    if remove_domain:
-        if not remove_domain.startswith("."):
-            remove_domain = f".{remove_domain}"
-        remove_len = len(remove_domain)
-    objectclass = get_objectclass()
-    dn = cfg["ldif"]["dn"]
-    encoding = cfg["default"].get("fileencoding", "")
+class HostGroupLDIF(LDIFBase[HostGroupLdifDataStorage]):
+    """Host group LDIF generator."""
 
-    for i in hostgroups:
-        cn = i["name"]
-        desc = i["description"] or None
-        if encoding == "ascii":
-            desc = common.LDIFutils.to_iso646_60(desc)
+    def __init__(self, app_config: Config) -> None:
+        super().__init__(app_config)
+        self.data = HostGroupLdifDataStorage(
+            hostgroups=LdifData(
+                name="hostgroups",
+                type=HostGroup,
+                default=[],
+                first_func=self.client.host_group.get_first,
+                get_func=self.client.host_group.get_list,
+                count_func=self.client.host_group.get_count,
+            )
+        )
+        self.domain: str = self.config.mreg.domain
+        if self.domain and not self.domain.startswith("."):
+            self.domain = f".{self.domain}"
 
-        entry = {
-            "dn": f"cn={cn},{dn}",
-            "cn": cn,
-            "description": desc,
-            "objectClass": objectclass,
+    @property
+    @override
+    def required_ldif_fields(self) -> set[str]:
+        return {"dn", "objectClass"}
+
+    @property
+    @override
+    def command(self) -> str:
+        return "hostgroup-ldif"
+
+    @property
+    @override
+    def command_config(self) -> HostGroupLdifConfig:
+        return self._app_config.hostgroup_ldif
+
+    @override
+    def create_ldif(self) -> io.StringIO:
+        """Create the LDIF file from fetched data."""
+        # Collect the LDIF entries
+        entries: list[HostGroupLdifEntry] = []
+        for hostgroup in self.data.hostgroups.data:
+            entry = self.hostgroup_to_ldif_entry(hostgroup)
+            entries.append(entry)
+
+        # Construct the string to write to the LDIF file
+        ldifs = io.StringIO()
+        if self.config.ldif.make_head_entry:
+            ldifs.write(entry_string(self.get_head_entry()))
+        for entry in entries:
+            ldifs.write(entry_string(entry))
+        return ldifs
+
+    def hostgroup_to_ldif_entry(self, hostgroup: HostGroup) -> HostGroupLdifEntry:
+        # Determine description before creating the entry to guarantee
+        # ordering of fields, so we maintain parity with the old script
+        if hostgroup.description:
+            if self.config.encoding == "ascii":
+                description = common.LDIFutils.to_iso646_60(hostgroup.description)
+            else:
+                description = hostgroup.description
+        else:
+            description = None
+        entry: HostGroupLdifEntry = {
+            "dn": f"cn={hostgroup.name},{self.config.ldif.dn}",
+            "cn": hostgroup.name,
+            "description": description,
+            "objectClass": self.config.ldif.objectClass,
+            "memberNisNetgroup": hostgroup.groups,  # omitted if empty
         }
-        if i["groups"]:
-            entry["memberNisNetgroup"] = [g["name"] for g in i["groups"]]
-        if i["hosts"]:
-            triple = []
-            for host in i["hosts"]:
-                hostname = host["name"]
-                if remove_domain and hostname.endswith(remove_domain):
-                    short = hostname[:-remove_len]
+
+        if hostgroup.hosts:
+            triple: list[str] = []
+            for hostname in hostgroup.hosts:
+                if self.domain and hostname.endswith(self.domain):
+                    # NOTE: this does not handle domains such as ifi.uio.no.
+                    # the resulting "short name" is then <hostname>.ifi
+                    # This behavior was present in the old script and is kept
+                    # for parity with that version. Should this be improved?
+                    short = hostname.removesuffix(self.domain)
                     triple.append(f"({short},-,)")
                 triple.append(f"({hostname},-,)")
             entry["nisNetgroupTriple"] = triple
-        ret.append(entry)
-
-    return ret
-
-
-@common.utils.timing
-def get_hostgroups(url):
-    return conn.get_list(url + "?page_size=1000")
-
-
-@common.utils.timing
-def hostgroup_ldif(args, url):
-    for i in (
-        "destdir",
-        "workdir",
-    ):
-        common.utils.mkdir(cfg["default"][i])
-
-    lockfile = os.path.join(cfg["default"]["workdir"], "lockfile")
-    lock = fasteners.InterProcessLock(lockfile)
-    if lock.acquire(blocking=False):
-        if common.utils.updated_entries(conn, url, "hostgroups.json") or args.force:
-            hostgroups = get_hostgroups(url)
-            create_ldif(hostgroups)
-            if "postcommand" in cfg["default"]:
-                common.utils.run_postcommand()
-        else:
-            logger.info("No updated hostgroups")
-        lock.release()
-    else:
-        logger.warning(f"Could not lock on {lockfile}")
+        return entry
 
 
 @app.command("hostgroup-ldif", help="Export hostgroups from mreg as a ldif.")
 def main(
     config: Annotated[
         str | None,
-        typer.Option(None, help="(DEPRECATED) path to config file", hidden=True),
+        typer.Option("--config", help="(DEPRECATED) path to config file", hidden=True),
     ] = None,
-    force: Annotated[
-        bool,
-        typer.Option("--force", help="force update"),
-    ] = False,
+    force_check: Annotated[
+        bool | None,
+        typer.Option("--force", "--force-check", help="force refresh of data from mreg"),
+    ] = None,
+    ignore_size_change: Annotated[
+        bool | None,
+        typer.Option(
+            "--ignore-size-change",
+            help="ignore size changes when writing the LDIF file",
+        ),
+    ] = None,
+    use_saved_data: Annotated[
+        bool | None,
+        typer.Option(
+            "--use-saved-data",
+            help="force use saved data from previous runs. Takes precedence over --force",
+        ),
+    ] = None,
+    filename: Annotated[
+        str | None,
+        typer.Option(
+            "--filename",
+            help="output filename for the ldif file",
+        ),
+    ] = None,
 ):
-    global cfg, conn, logger
+    # Get config and add overrides from command line
+    conf = app.get_config()
+    if force_check is not None:
+        conf.hostgroup_ldif.force_check = force_check
+    if ignore_size_change is not None:
+        conf.hostgroup_ldif.ignore_size_change = ignore_size_change
+    if use_saved_data is not None:
+        conf.hostgroup_ldif.use_saved_data = use_saved_data
+    if filename is not None:
+        conf.hostgroup_ldif.filename = filename
 
-    cfg = configparser.ConfigParser()
-    cfg.optionxform = str
-    cfg.read(config or "hostgroup-ldif.conf")
-
-    for i in ("default", "mreg", "ldif"):
-        if i not in cfg:
-            error(f"Missing section {i} in config file", os.EX_CONFIG)
-
-    if "filename" not in cfg["default"]:
-        error("Missing 'filename' in default section in config file", os.EX_CONFIG)
-
-    common.utils.cfg = cfg
-    logger = common.utils.getLogger()
-    conn = common.connection.Connection(cfg["mreg"])
-    url = requests.compat.urljoin(cfg["mreg"]["url"], "/api/v1/hostgroups/")
-    hostgroup_ldif(args, url)
+    configure_logging(conf)
+    ldif = HostGroupLDIF(conf)
+    with app.lock(ldif.config.workdir, "hosts_ldif"):
+        ldif.run()
+    ldif.create_ldif()
 
 
 if __name__ == "__main__":
