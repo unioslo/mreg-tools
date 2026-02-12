c45a2c8e6ff78d02f39b42e0704eea24cc678e3f
pederhan <pederhan@usit.uio.no>
2026-02-12 16:14:21 +0100

    Add get-hostpolicy

--- mreg_tools/commands/get_hostpolicy.py
+++ mreg_tools/commands/get_hostpolicy.py
@@ -1,135 +1,174 @@
 from __future__ import annotations
 
-import configparser
 import io
-import os
 from collections import defaultdict
 from typing import Annotated
+from typing import Final
+from typing import final
+from typing import override
 
-import fasteners
-import requests
+import structlog.stdlib
 import typer
+from mreg_api.models import Atom
+from mreg_api.models import Role
 
-from mreg_tools import common
 from mreg_tools.app import app
-from mreg_tools.common.utils import error
+from mreg_tools.common.base import CommandBase
+from mreg_tools.common.base import MregData
+from mreg_tools.common.base import MregDataStorage
+from mreg_tools.config import Config
+from mreg_tools.config import GetHostPolicyConfig
+
+COMMAND_NAME: Final[str] = "get-hostpolicy"
+logger = structlog.stdlib.get_logger(command=COMMAND_NAME)
 
 
-def write_file(filename, f):
-    filename = os.path.join(cfg["default"]["destdir"], filename)
-    common.utils.write_file(filename, f)
+class HostDataStorage(MregDataStorage):
+    """Storage for fetched host data."""
+
+    def __init__(self, roles: MregData[Role], atoms: MregData[Atom]) -> None:
+        self.roles = roles
+        self.atoms = atoms
 
 
-def create_atoms(atoms):
-    f = io.StringIO()
-    for atom in atoms:
-        f.write(
-            "{};{};;{}\n".format(atom["name"], atom["description"], atom["create_date"])
+@final
+class GetHostPolicy(CommandBase[HostDataStorage]):
+    """get-hostpolicy command class."""
+
+    def __init__(self, app_config: Config):
+        super().__init__(app_config)
+        self.data = HostDataStorage(
+            roles=MregData(
+                name="roles",
+                type=Role,
+                default=[],
+                first_func=self.client.role.get_first,
+                get_func=self.client.role.get_list,
+                count_func=self.client.role.get_count,
+            ),
+            atoms=MregData(
+                name="atoms",
+                type=Atom,
+                default=[],
+                first_func=self.client.atom.get_first,
+                get_func=self.client.atom.get_list,
+                count_func=self.client.atom.get_count,
+            ),
         )
-    write_file("atoms.csv", f)
 
+    @property
+    @override
+    def command(self) -> str:
+        return COMMAND_NAME
 
-def create_roles(role_data):
-    relationships = io.StringIO()
-    roles = io.StringIO()
-    hostpolicies = io.StringIO()
+    @property
+    @override
+    def command_config(self) -> GetHostPolicyConfig:
+        return self._app_config.get_hostpolicy
 
-    host2roles = defaultdict(list)
+    @override
+    def run(self) -> None:
+        self.create_role_csv(self.data.roles.data)
+        self.create_atom_csv(self.data.atoms.data)
 
-    for role in role_data:
-        role_name = role["name"]
-        atoms = [i["name"] for i in role["atoms"]]
-        for host in role["hosts"]:
-            host2roles[host["name"] + "."].append(role_name)
+    def role_csv_str(self, role: Role) -> str:
+        """Create a CSV-formatted line for a given role."""
+        return (
+            f"{role.name};{role.description};;"  # NOTE: why double ;;?
+            f"{role.created_at};{','.join(role.atoms)}\n"
+        )
 
+    def role_atom_csv_str(self, role: Role, atom: str) -> str:
+        """Create a CSV-formatted line for a role-atom relationship."""
+        return f"{role.name};hostpol_contains;{atom}\n"
+
+    def host_roles_csv_str(self, host: str, policies: list[str]) -> str:
+        """Create a CSV-formatted line for a host and its associated roles."""
+        return f"{host};{','.join(policies)}\n"
+
+    def create_atom_csv(self, atoms: list[Atom]) -> None:
+        """Create the CSV file for atoms."""
+        atom_csv = io.StringIO()
         for atom in atoms:
-            relationships.write(f"{role_name};hostpol_contains;{atom}\n")
-        roles.write(
-            "{};{};;{};{}\n".format(
-                role_name, role["description"], role["create_date"], ",".join(atoms)
-            )
-        )
+            atom_csv.write(f"{atom.name};{atom.description};;{atom.created_at}\n")
+        self.write(atom_csv, filename="atoms.csv")
 
-    for host in sorted(host2roles):
-        host_roles = host2roles[host]
-        hostpolicies.write("{};{}\n".format(host, ",".join(host_roles)))
+    def create_role_csv(
+        self,
+        roles: list[Role],
+    ) -> None:
+        """Create the CSV files for roles, hostpolicies, and role:atom relationships."""
+        # String builders for the three CSV files
+        roles_csv = io.StringIO()
+        policies_csv = io.StringIO()
+        relationships_csv = io.StringIO()
 
-    write_file("hostpolicies.csv", hostpolicies)
-    write_file("relationships.csv", relationships)
-    write_file("roles.csv", roles)
+        # Map of hosts to their associated roles, used to build hostpolicies.csv
+        host_map = defaultdict[str, list[str]](list)
 
+        for role in roles:
+            # Build roles.csv
+            roles_csv.write(self.role_csv_str(role))
 
-@common.utils.timing
-def get_atoms(url):
-    return conn.get_list(url + "?page_size=1000")
+            # Build relationships.csv
+            for atom in role.atoms:
+                relationships_csv.write(self.role_atom_csv_str(role, atom))
 
+            for host in role.hosts:  # Add role to host map for each host
+                host_map[host].append(role.name)
 
-@common.utils.timing
-def get_roles(url):
-    return conn.get_list(url + "?page_size=1000")
+        # Build hostpolicies.csv
+        for host in sorted(host_map):
+            role_names = host_map[host]
+            policies_csv.write(self.host_roles_csv_str(host, role_names))
 
-
-@common.utils.timing
-def dump_hostpolicies(force):
-    for i in (
-        "destdir",
-        "workdir",
-    ):
-        common.utils.mkdir(cfg["default"][i])
-
-    atoms_url = requests.compat.urljoin(cfg["mreg"]["url"], "/api/v1/hostpolicy/atoms/")
-    roles_url = requests.compat.urljoin(cfg["mreg"]["url"], "/api/v1/hostpolicy/roles/")
-
-    lockfile = os.path.join(cfg["default"]["workdir"], "lockfile")
-    lock = fasteners.InterProcessLock(lockfile)
-    if lock.acquire(blocking=False):
-        updated = False
-        if common.utils.updated_entries(conn, atoms_url, "atoms.json") or force:
-            atoms = get_atoms(atoms_url)
-            create_atoms(atoms)
-            updated = True
-        if common.utils.updated_entries(conn, roles_url, "roles.json") or force:
-            roles = get_roles(roles_url)
-            create_roles(roles)
-            updated = True
-
-        if updated and "postcommand" in cfg["default"]:
-            common.utils.run_postcommand()
-        else:
-            logger.info("No updated atoms or roles")
-        lock.release()
-    else:
-        logger.warning(f"Could not lock on {lockfile}")
+        self.write(roles_csv, filename="roles.csv")
+        self.write(policies_csv, filename="hostpolicies.csv")
+        self.write(relationships_csv, filename="relationships.csv")
 
 
-@app.command("get-hostpolicy", help="Export hostpolicies from mreg as a textfiles.")
+@app.command(COMMAND_NAME, help="Export host info from mreg as a textfiles.")
 def main(
-    config: Annotated[
-        str | None,
-        typer.Option(None, help="(DEPRECATED) path to config file", hidden=True),
+    force_check: Annotated[
+        bool | None,
+        typer.Option("--force", "--force-check", help="force refresh of data from mreg"),
     ] = None,
-    force: Annotated[
-        bool,
-        typer.Option("--force", help="force update"),
-    ] = False,
+    ignore_size_change: Annotated[
+        bool | None,
+        typer.Option(
+            "--ignore-size-change",
+            help="ignore size changes when writing the LDIF file",
+        ),
+    ] = None,
+    use_saved_data: Annotated[
+        bool | None,
+        typer.Option(
+            "--use-saved-data",
+            help="force use saved data from previous runs. Takes precedence over --force",
+        ),
+    ] = None,
+    filename: Annotated[
+        str | None,
+        typer.Option(
+            "--filename",
+            help="output filename for the ldif file",
+        ),
+    ] = None,
 ):
-    global cfg, conn, logger
+    # Get config and add overrides from command line
+    conf = app.get_config()
+    if force_check is not None:
+        conf.get_hostpolicy.force_check = force_check
+    if ignore_size_change is not None:
+        conf.get_hostpolicy.ignore_size_change = ignore_size_change
+    if use_saved_data is not None:
+        conf.get_hostpolicy.use_saved_data = use_saved_data
+    if filename is not None:
+        conf.get_hostpolicy.filename = filename
 
-    cfg = configparser.ConfigParser()
-    cfg.optionxform = str
-    cfg.read(config or "get-hostpolicy.conf")
-
-    for i in (
-        "default",
-        "mreg",
-    ):
-        if i not in cfg:
-            error(f"Missing section {i} in config file", os.EX_CONFIG)
-
-    common.utils.cfg = cfg
-    logger = common.utils.getLogger()
-    conn = common.connection.Connection(cfg["mreg"])
-    dump_hostpolicies(force)
+    cmd = GetHostPolicy(conf)
+    with app.lock(cmd.config.workdir, COMMAND_NAME):
+        cmd()
 
 
 if __name__ == "__main__":
