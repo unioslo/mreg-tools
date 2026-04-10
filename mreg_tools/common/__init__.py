2869d3878ff18110de361308cadab7212cb75207
pederhan <pederhan@usit.uio.no>
2026-04-10 11:50:46 +0200

    Remove unused common.connection module

--- mreg_tools/common/__init__.py
+++ mreg_tools/common/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
 from mreg_tools.common import base
-from mreg_tools.common import connection
 from mreg_tools.common import ldif
 from mreg_tools.common import utils
 
 __all__ = [
     "base",
-    "connection",
     "ldif",
     "utils",
 ]
